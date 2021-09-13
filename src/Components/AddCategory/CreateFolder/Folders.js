9ee1c71e29991334b051528434638e825862e5f3
Francisco Veiras <74626997+francisco-svg761@users.noreply.github.com>
2021-09-13 16:24:35 -0300

    pages

--- src/Components/AddCategory/CreateFolder/Folders.js
+++ src/Components/AddCategory/CreateFolder/Folders.js
@@ -1,49 +1,48 @@
 import React from 'react';
-import {Tr,
-        Td,
-        Tbody,
-        Count,
-        TrFolder,
-        Arrow
-} from './FolderStyle';
+import { Tr, Td, Tbody, Count, TrFolder } from './FolderStyle';
 import { useState } from 'react';
 
 export const Folders = ({ categorie, catDescription, info }) => {
+  const [open, setOpen] = useState(false);
 
-    const [open, setOpen] = useState(false)
-
-    const openFolder = () => {
-        if (open === false) {
-            setOpen(!false)
-        }else {
-            setOpen(!true)
-        }
+  const openFolder = () => {
+    if (open === false) {
+      setOpen(!false);
+    } else {
+      setOpen(!true);
     }
+  };
 
-    return (
-        <>
-        {
-            <Tbody key={categorie}>
-                        <Tr onClick={openFolder}>
-                            <Td>{categorie}</Td>
-                            <Td>{catDescription}</Td>
-                            <Td></Td>
-                        </Tr>
-                        {
-                            info.filter(cy => cy.folder === categorie).map(cy => 
-                                <TrFolder key={cy.folder} open={open}>
-                                    <Td>{cy.description}</Td>
-                                    <Td>
-                                        <Count>{("0" + Math.floor((cy.time / 3600000) % 60)).slice(-2)}:</Count>
-                                        <Count>{("0" + Math.floor((cy.time / 60000) % 60)).slice(-2)}:</Count>
-                                        <Count>{("0" + Math.floor((cy.time/ 1000) % 60)).slice(-2)}</Count>
-                                    </Td>
-                                    <Td>{cy.pages} Pages</Td>
-                                </TrFolder>
-                            )
-                        }
-                    </Tbody>
-        }
-        </>
-    )
-}
+  return (
+    <>
+      {
+        <Tbody key={categorie}>
+          <Tr onClick={openFolder}>
+            <Td>{categorie}</Td>
+            <Td>{catDescription}</Td>
+            <Td></Td>
+          </Tr>
+          {info
+            .filter((cy) => cy.folder === categorie)
+            .map((cy) => (
+              <TrFolder key={cy.folder} open={open}>
+                <Td>{cy.description}</Td>
+                <Td>
+                  <Count>
+                    {('0' + Math.floor((cy.time / 3600000) % 60)).slice(-2)}:
+                  </Count>
+                  <Count>
+                    {('0' + Math.floor((cy.time / 60000) % 60)).slice(-2)}:
+                  </Count>
+                  <Count>
+                    {('0' + Math.floor((cy.time / 1000) % 60)).slice(-2)}
+                  </Count>
+                </Td>
+                <Td>{cy.pages} Pages</Td>
+              </TrFolder>
+            ))}
+        </Tbody>
+      }
+    </>
+  );
+};
